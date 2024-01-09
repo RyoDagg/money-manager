b7ce20f572fa52c398e944bda4c4519f92d98cc9
Gaddour <mannai.abdlkader@gmail.com>
2024-01-09 16:57:23 +0100

    fixed local Storage issue and going to start working on classes

--- assets/scripts/transactions.js
+++ assets/scripts/transactions.js
@@ -6,10 +6,10 @@ var storedTransactions;
 // }
 
 if (localStorage.getItem("storedTransactions")) {
-    storedTransactions = JSON.parse(localStorage.getItem("storedTransactions"));
-  } else {
-    storedTransactions = [];
-  }
+  storedTransactions = JSON.parse(localStorage.getItem("storedTransactions"));
+} else {
+  storedTransactions = [];
+}
 
 var $type = $("#type");
 var $acc = $("#acc");
@@ -17,6 +17,7 @@ var $categ = $("#categ");
 var $ammount = $("#ammount");
 var $submit = $("#submit");
 var $transactions = $("#transactions");
+
 var tr = {
   type: $type.val(),
   acc: $acc.val(),
@@ -43,5 +44,8 @@ $submit.on("click", function () {
     ammount: $ammount.val(),
   });
 
-  localStorage.setItem("storedTransactions", storedTransactions);
+  localStorage.setItem(
+    "storedTransactions",
+    JSON.stringify(storedTransactions)
+  );
 });
