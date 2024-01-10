99420f039c1bc6a83dedc5d4855750eb830301f7
Gaddour <mannai.abdlkader@gmail.com>
2024-01-10 09:00:10 +0100

    Filter Done

--- assets/scripts/filters.js
+++ assets/scripts/filters.js
@@ -1,5 +1,7 @@
 var $filter = {
   category: $("#category-filter"),
+  account: $("#account-filter"),
+  live: $("#live-search"),
 };
 
 function filterCategory(category) {
@@ -14,6 +16,49 @@ function filterCategory(category) {
   });
 }
 
+each(storedAccounts, function (account) {
+  $option = $("<option></option>").val(account.id).text(account.name);
+  $filter.account.append($option);
+  // $filter.to.append($option.clone());
+});
+
+function filterAccount(account) {
+  var filtered = filter(storedTransactions, function (transaction) {
+    return transaction.from === account || transaction.to === account;
+  });
+
+  $transactions.empty();
+
+  each(filtered, function (transaction, i) {
+    transaction.render();
+  });
+}
+
+function filterLive(query) {
+  var q = query.toUpperCase();
+  var filtered = filter(storedTransactions, function (transaction) {
+    return (
+      transaction.fromName.toUpperCase().includes(q) ||
+      transaction.toName.toUpperCase().includes(q) ||
+      transaction.category.toUpperCase().includes(q) ||
+      transaction.type.toUpperCase().includes(q)
+    );
+  });
+
+  $transactions.empty();
+
+  each(filtered, function (transaction, i) {
+    transaction.render();
+  });
+}
+
 $filter.category.on("change", function () {
-  filterCategory($filter.category.val());
+  filterAccount($filter.category.val());
+});
+
+$filter.account.on("change", function () {
+  filterAccount($filter.account.val());
+});
+$filter.live.keypress(function () {
+  filterLive($filter.live.val());
 });
